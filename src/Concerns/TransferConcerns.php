5d8f892e3109401a20ecd1e49d3ea4db1b79b7fe
eric <yybawang@gmail.com>
2026-03-10 00:20:31 +0800

    允许负数金额

--- src/Concerns/TransferConcerns.php
+++ src/Concerns/TransferConcerns.php
@@ -25,10 +25,10 @@ trait TransferConcerns
             return 0;
         }
         $reason = $this->getReason($reason_code);
-        $wallet = $this->getWallet($user_id, $reason->currency_id, $reason->identity_id);
-        $transfer_id = Cache::lock('ebank@transfer:'.$wallet->id)->block(10, fn() => DB::transaction(function() use ($user_id, $amount, $reason, $description, $business_data){
+        $transfer_id = Cache::lock('ebank@transfer:'.$user_id.':'.$reason->currency_id.':'.$reason->identity_id)->block(10, fn() => DB::transaction(function() use ($user_id, $amount, $reason, $description, $business_data){
             $wallet = $this->getWallet($user_id, $reason->currency_id, $reason->identity_id);
-            abort_if($amount < 0 && $wallet->balance < -$amount, 422, '钱包 '.$wallet->id.' 可用余额不足');
+            // 线程互抢时，如果有两笔入款两笔扣款，可能先扣款两笔导致异常退出而余额对不上，所以这里需允许为负数
+//            abort_if($amount < 0 && $wallet->balance < -$amount, 422, '钱包 '.$wallet->id.' 可用余额不足');
             $wallet->increment('balance', $amount); // increment 也是兼容负数的
             $transfer = Transfer::create([
                 'user_id' => $user_id,
@@ -64,13 +64,11 @@ trait TransferConcerns
         }
         $from_reason = $this->getReason($from_reason_code);
         $to_reason = $this->getReason($to_reason_code);
-        $from_wallet = $this->getWallet($from_user_id, $from_reason->currency_id, $from_reason->identity_id);
-        $to_wallet = $this->getWallet($to_user_id, $to_reason->currency_id, $to_reason->identity_id);
-        Cache::lock('ebank@faceToFace:'.$from_wallet->id.':'.$to_wallet->id)->block(10, fn() => DB::transaction(function() use ($from_user_id, $to_user_id, $amount, $from_reason, $to_reason, $description, $business_data){
+        Cache::lock('ebank@faceToFace:'.'ebank@transfer:'.$from_user_id.':'.$to_user_id.':'.$from_reason->currency_id.':'.$from_reason->identity_id.':'.$to_reason->currency_id.':'.$to_reason->identity_id)->block(10, fn() => DB::transaction(function() use ($from_user_id, $to_user_id, $amount, $from_reason, $to_reason, $description, $business_data){
             $from_wallet = $this->getWallet($from_user_id, $from_reason->currency_id, $from_reason->identity_id);
             $to_wallet = $this->getWallet($to_user_id, $to_reason->currency_id, $to_reason->identity_id);
-            abort_if($amount > 0 && $from_wallet->balance < $amount, 422, '钱包 '.$from_wallet->id.' 可用余额不足');
-            abort_if($amount < 0 && $to_wallet->balance < -$amount, 422, '钱包 '.$to_wallet->id.' 可用余额不足');
+//            abort_if($amount > 0 && $from_wallet->balance < $amount, 422, '钱包 '.$from_wallet->id.' 可用余额不足');
+//            abort_if($amount < 0 && $to_wallet->balance < -$amount, 422, '钱包 '.$to_wallet->id.' 可用余额不足');
             $from_wallet->decrement('balance', $amount);
             $to_wallet->increment('balance', $amount);
 
